3e15d91a9ed8fdf85725315dbbbaff051f3b6bd1
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-05-17 08:30:51 +0300

    Added Task 2 aggregations and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -23,6 +23,19 @@ SELECT
     p.name AS property_name,
     p.location,
     COUNT(b.booking_id) AS booking_count,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name, p.location
+ORDER BY booking_rank, p.property_id;
+
+
+-- using ROW_NUMBER
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    COUNT(b.booking_id) AS booking_count,
     ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
